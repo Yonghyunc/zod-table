ab2e028a529c172616fe1e51bb7dec38f8df92ac
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2026-09-05 11:49:27 +0900

    feat: add snack meal time

--- constants/meal.ts
+++ constants/meal.ts
@@ -1,7 +1,10 @@
-export const MEAL_TIME: Record<string, string> = {
+export const MEAL_TIMES = ["breakfast", "lunch", "dinner", "snack"] as const;
+
+export type MealTime = (typeof MEAL_TIMES)[number];
+
+export const MEAL_TIME: Record<MealTime, string> = {
   breakfast: "🌅 아침",
   lunch: "️🌤️ 점심",
   dinner: "🌙 저녁",
+  snack: "🍎 간식",
 };
-
-export type MealTime = keyof typeof MEAL_TIME;
